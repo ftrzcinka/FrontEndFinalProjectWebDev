3b54701f3b82d8befda17f38d5d5da68ad844cce
Josiel Perez <josielp0218@gmail.com>
2023-04-29 18:58:32 -0400

    added draft design of nav bar and routes for the buttons

---
+++ src/pages/taskpage/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Styles from "./styles.module.css";
+
+function Taskpage() {
+  return (
+    <>
+      <div>
+        <h1>Task Page</h1>
+      </div>
+    </>
+  );
+}
+
+export default Taskpage;
