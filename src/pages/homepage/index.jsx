ee8ea82187f0868f37b7d0eec2769ec0c471ca5f
Remos96 <omer122@protonmail.com>
2023-05-05 17:34:03 -0400

    Made it more like a NavBar

--- src/pages/homepage/index.jsx
+++ src/pages/homepage/index.jsx
@@ -4,40 +4,30 @@ import { Link } from "react-router-dom";
 
 function Homepage() {
   return (
-    <>
-      <div>
-        <div>
-          <div className={Styles.navContainer}>
-            <Link
-              to="/tasks"
-              style={{ textDecoration: "none", color: "black" }}
-            >
-              <div className={Styles.taskContainer}>TASKS</div>
-            </Link>
-            <Link
-              to="/employees"
-              style={{ textDecoration: "none", color: "black" }}
-            >
-              <div className={Styles.employeesContainer}>EMPLOYEES</div>
-            </Link>
-          </div>
-          <div className={Styles.boxNavContainer}>
-            <Link
-              to="/tasks"
-              style={{ textDecoration: "none", color: "black" }}
-            >
-              <div className={Styles.taskContainerBox}>TASKS</div>
-            </Link>
-            <Link
-              to="/employees"
-              style={{ textDecoration: "none", color: "black" }}
-            >
-              <div className={Styles.employeesContainerBox}>EMPLOYEES</div>
-            </Link>
-          </div>
-        </div>
+    <div>
+      <div className={Styles.navContainer}>
+        <Link to="/tasks" style={{ textDecoration: "none", color: "black" }}>
+          <div className={Styles.taskContainer}>TASKS</div>
+        </Link>
+        <Link
+          to="/employees"
+          style={{ textDecoration: "none", color: "black" }}
+        >
+          <div className={Styles.employeesContainer}>EMPLOYEES</div>
+        </Link>
       </div>
-    </>
+      <div className={Styles.boxNavContainer}>
+        <Link to="/tasks" style={{ textDecoration: "none", color: "black" }}>
+          <div className={Styles.taskContainerBox}>TASKS</div>
+        </Link>
+        <Link
+          to="/employees"
+          style={{ textDecoration: "none", color: "black" }}
+        >
+          <div className={Styles.employeesContainerBox}>EMPLOYEES</div>
+        </Link>
+      </div>
+    </div>
   );
 }
 
