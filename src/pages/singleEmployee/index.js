3a38b59f11ef79e1d31374a06a0534c23f8ca154
ftrzcinka <ftrzcinka@gmail.com>
2023-05-09 23:11:55 -0400

    view button for taskpage, add navbars

--- src/pages/singleEmployee/index.js
+++ src/pages/singleEmployee/index.js
@@ -6,6 +6,7 @@ import TaskCard from "../../components/TaskCard";
 import Styles from "./styles.module.css";
 import { useParams } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
+import Navbar from "../../components/Navbar";
 
 export default function SingleEmployee() {
   const [tasks, setTasks] = useState([]);
@@ -46,73 +47,79 @@ export default function SingleEmployee() {
   }, []);
 
   return (
-    <div className={Styles.employeeList}>
-      {employee && (
-        <div key={employee.id}>
-          <EmployeeCard
-            id={employee.id}
-            firstName={employee.firstname}
-            lastName={employee.lastname}
-            department={employee.department}
-            refreshEmployees={refreshEmployees}
-            refreshOneEmployee={() => refreshOneEmployee(employee.id)}
-          />
-          <button
-            onClick={async () => {
-              await EmployeeAPI.deleteEmployee(employee.id);
-              refreshEmployees(employee.id);
-              refreshOneEmployee(employee.id);
-            }}
-          >
-            Delete User
-          </button>
-          {/* <button onClick={(employee) => handleGoEmployeePage(employee.id)}>
-                View Employee
-              </button> */}
-          <br></br>
-          <br></br>
-          <br></br>
-          {tasks.map((task) => {
-            if (task.employeeId === employee.id) {
-              return (
-                <div key={task.id}>
-                  <TaskCard
-                    id={task.id}
-                    taskDescription={task.description}
-                    taskPriority={task.priority}
-                    taskCompleted={task.completed}
-                    taskEmployeeId={task.employeeId}
-                    refreshTasks={refreshTasks}
-                    getSingleTask={getSingleTask}
-                  />
-                  <button
-                    onClick={async () => {
-                      await TaskAPI.deleteTask(task.id);
-                      refreshTasks();
-                    }}
-                  >
-                    Delete Task
-                  </button>
-                  <button
-                    onClick={async () => {
-                      await TaskAPI.unassignTask(task.id);
-                      refreshTasks();
-                    }}
-                  >
-                    Unassign Task
-                  </button>
-                  <button onClick={() => handleGoTaskPage(task.id)}>
-                    View Task
-                  </button>
-                  <br></br>
-                  <br></br>
-                </div>
-              );
-            }
-          })}
-        </div>
-      )}
-      {!employee && <h3>No Employees</h3>}
+    <div>
+      <Navbar />
+      <div className={Styles.employeeList}>
+        {employee && (
+          <div key={employee.id}>
+            <EmployeeCard
+              id={employee.id}
+              firstName={employee.firstname}
+              lastName={employee.lastname}
+              department={employee.department}
+              refreshEmployees={refreshEmployees}
+              refreshOneEmployee={() => refreshOneEmployee(employee.id)}
+            />
+            <button
+              onClick={async () => {
+                await EmployeeAPI.deleteEmployee(employee.id);
+                refreshEmployees(employee.id);
+                refreshOneEmployee(employee.id);
+                window.location.reload(false);
+              }}
+            >
+              Delete User
+            </button>
+            {/* <button onClick={(employee) => handleGoEmployeePage(employee.id)}>
+              View Employee
+            </button> */}
+            <br></br>
+            <br></br>
+            <br></br>
+            {tasks.map((task) => {
+              if (task.employeeId === employee.id) {
+                return (
+                  <div key={task.id}>
+                    <TaskCard
+                      id={task.id}
+                      taskDescription={task.description}
+                      taskPriority={task.priority}
+                      taskCompleted={task.completed}
+                      taskEmployeeId={task.employeeId}
+                      refreshTasks={refreshTasks}
+                      getSingleTask={getSingleTask}
+                    />
+                    <button
+                      onClick={async () => {
+                        await TaskAPI.deleteTask(task.id);
+                        refreshTasks();
+                        window.location.reload(false);
+                      }}
+                    >
+                      Delete Task
+                    </button>
+                    <button
+                      onClick={async () => {
+                        await TaskAPI.unassignTask(task.id);
+                        refreshTasks();
+                        window.location.reload(false);
+                      }}
+                    >
+                      Unassign Task
+                    </button>
+                    <button onClick={() => handleGoTaskPage(task.id)}>
+                      View Task
+                    </button>
+                    <br></br>
+                    <br></br>
+                  </div>
+                );
+              }
+            })}
+          </div>
+        )}
+        {!employee && <h3>No Employees Of Such ID Number</h3>}
+      </div>
     </div>
   );
 }
